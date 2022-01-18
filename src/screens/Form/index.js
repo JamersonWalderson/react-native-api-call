941df34b4aed69d92cefac76709480f3b8e83a3a
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-01-18 11:27:22 -0300

    estilizando página do formulário

--- src/screens/Form/index.js
+++ src/screens/Form/index.js
@@ -1,11 +1,56 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Button } from "react-native-elements";
 
-export default function ScreenForm() {
+export default function ScreenForm({navigation, route}) {
+
+    const [user, setUser] = useState(route.params ? route.params : {});
+    
+    // Onpress buttom
+    const UserSave = () =>{
+        navigation.goBack();
+    }
 
     return (
-        <View>
-            <Text>Tela de formulário</Text>
+        <View style={styles.form}>
+            <Text>Nome:</Text>
+            <TextInput 
+                placeholder="Informe o nome"
+                // onChangeText={user}
+                // value={user}
+                style={styles.input}
+            />
+
+            <Text>Email:</Text>
+            <TextInput 
+                placeholder="Informe o e-mail"
+                // onChangeText={user}
+                // value={user}
+                style={styles.input}
+            />
+
+            <Text>URL do avatar:</Text>
+            <TextInput 
+                placeholder="Informe a URL do avatar"
+                // onChangeText={user}
+                // value={user}
+                style={styles.input}
+            />
+            <Button 
+                title="Salvar"
+                onPress={UserSave}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10
+    }
+});
