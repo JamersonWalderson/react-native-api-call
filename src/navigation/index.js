96d90d7bfae89a0062798f58654ddcaeeef84647
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2022-01-19 15:10:34 -0300

    app finalizado

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button, Icon } from "react-native-elements";
-import { StyleSheet } from "react-native";
+import { Linking, StyleSheet } from "react-native";
 import ScreenHome from '../screens/Home';
 import ScreenForm from '../screens/Form';
 import colors from '../utils/colors';
@@ -16,13 +16,22 @@ const Router = (props) => {
     const ButtonRight = ({ navigation, route }) => {
         // console.warn(Object.keys(route));
 
+        const openUrl = () => {
+            Linking.openURL("https://github.com/JamersonWalderson/react-native-screens");
+        }
+
         return ({
             title: "Lista de usuários",
             headerRight: () => (
                 <Button 
                     type="clear"
-                    icon={<Icon name="add" size={25} color="#fff"/>}
-                    onPress={() => navigation.navigate("Form")}
+                    icon={{
+                        name: 'github',
+                        type: 'font-awesome',
+                        size: 35,
+                        color: 'white',
+                      }}
+                    onPress={openUrl}
                 />
             )
 
@@ -31,9 +40,9 @@ const Router = (props) => {
 
     return(
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+                <Stack.Navigator screenOptions={screenOptions}>
                     <Stack.Screen name="Home" component={ScreenHome} options={ButtonRight}/>
-                    <Stack.Screen name="Form" component={ScreenForm} options={{title: "Formulário de cadastro"}} />
+                    {/* <Stack.Screen name="Form" component={ScreenForm} options={{title: "Formulário de cadastro"}} /> */}
                 </Stack.Navigator>
             </NavigationContainer>
 
